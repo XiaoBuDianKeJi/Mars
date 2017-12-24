ea93254617efc33fb15886602434d7786090f0b4
GM1598078574 <1598078574@qq.com>
2017-12-24 23:33:30 +0800

    PageHelper分页插件集成
    PageHelper分页插件集成

--- Biz/src/main/java/com/xiaobudiankeji/biz/controller/IndexController.java
+++ Biz/src/main/java/com/xiaobudiankeji/biz/controller/IndexController.java
@@ -1,10 +1,15 @@
 package com.xiaobudiankeji.biz.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.xiaobudiankeji.base.entity.Menu;
 import com.xiaobudiankeji.biz.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by zuoqing on 2017/12/24.
@@ -26,6 +31,23 @@ public class IndexController {
     public Object findUserMenuList(){
 
         return userService.findUserMenuList("李文涛");
+    }
+
+    /**
+     *
+     * 集成分页插件
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @RequestMapping("findUserMenuPageList")
+    public Object findUserMenuPageList(@RequestParam(value = "page", required = false, defaultValue = "1") int page,
+                                   @RequestParam(value = "pageSize", required = false, defaultValue = "2") int pageSize){
+
+        PageHelper.startPage(page, pageSize);//设置分页参数
+        List<Menu> humorList = this.userService.findUserMenuList("李文涛");
+        PageInfo<Menu> pageInfo = new PageInfo<>(humorList);
+        return pageInfo;
     }
 
 }
