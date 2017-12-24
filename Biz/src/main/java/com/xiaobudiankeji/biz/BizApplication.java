5cccd886744d92fde6b76518ed4d2ac532a0d798
GM1598078574 <1598078574@qq.com>
2017-12-24 17:13:56 +0800

    11
    11

--- Biz/src/main/java/com/xiaobudiankeji/biz/BizApplication.java
+++ Biz/src/main/java/com/xiaobudiankeji/biz/BizApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 @MapperScan(basePackages = {"com.xiaobudiankeji.base.dao"})
 public class BizApplication {
